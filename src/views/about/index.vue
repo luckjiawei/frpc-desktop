a513e5dfc272cf4cdfac136d09fc6be206af2c06
forestxieCode <1397798719@qq.com>
2024-08-07 20:27:27 +0800

    perf:优化样式

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -31,19 +31,21 @@ defineComponent({
 <template>
   <div class="main">
     <breadcrumb/>
-    <div
-      class="w-full h-full bg-white p-4 rounded drop-shadow-lg overflow-y-auto flex justify-center items-center flex-col"
-    >
-      <img src="/logo/only/1024x1024.png" class="w-[95px] h-[95px] mt-[-50px]" alt="Logo"/>
-      <div class="mt-[8px] text-2xl">Frpc Desktop</div>
-      <div class="mt-[8px] text-neutral-400 flex items-center">
-        v{{pkg.version}}
-        <Icon class="ml-1.5 cursor-pointer" icon="material-symbols:refresh-rounded"></Icon>
-      </div>
-      <div class="mt-[8px] text-sm">更简单的内网穿透工具！免费开源 / 桌面客户端 / 开机启动</div>
-      <div class="mt-[12px]">
-        <el-button type="primary" @click="handleCopyInfo">复制信息</el-button>
-        <el-button type="danger" plain @click="handleOpenGitHub">反馈问题</el-button>
+    <div class="app-container-breadcrumb">
+      <div
+        class="w-full h-full bg-white p-4 rounded drop-shadow-lg overflow-y-auto flex justify-center items-center flex-col"
+      >
+        <img src="/logo/only/1024x1024.png" class="w-[95px] h-[95px] mt-[-50px]" alt="Logo"/>
+        <div class="mt-[8px] text-2xl">Frpc Desktop</div>
+        <div class="mt-[8px] text-neutral-400 flex items-center">
+          v{{pkg.version}}
+          <Icon class="ml-1.5 cursor-pointer" icon="material-symbols:refresh-rounded"></Icon>
+        </div>
+        <div class="mt-[8px] text-sm">更简单的内网穿透工具！免费开源 / 桌面客户端 / 开机启动</div>
+        <div class="mt-[12px]">
+          <el-button type="primary" @click="handleCopyInfo">复制信息</el-button>
+          <el-button type="danger" plain @click="handleOpenGitHub">反馈问题</el-button>
+        </div>
       </div>
     </div>
   </div>
