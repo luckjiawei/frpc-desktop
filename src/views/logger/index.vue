179da0bbf696e8c7d293424358b848c3107ce70d
刘嘉伟 <8473136@qq.com>
2024-08-16 22:19:39 +0800

    :lipstick: 修复日志滚动条

--- src/views/logger/index.vue
+++ src/views/logger/index.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import { defineComponent, onMounted, onUnmounted, ref } from "vue";
+import {defineComponent, onMounted, onUnmounted, ref} from "vue";
 import Breadcrumb from "@/layout/compoenets/Breadcrumb.vue";
-import { ipcRenderer } from "electron";
+import {ipcRenderer} from "electron";
 
 defineComponent({
   name: "Logger"
@@ -11,21 +11,21 @@ const loggerContent = ref('<div class="text-white">暂无日志</div>');
 
 const handleLog2Html = (logContent: string) => {
   const logs = logContent
-    .split("\n")
-    .filter(f => f)
-    .map(m => {
-      if (m.indexOf("[E]") !== -1) {
-        return `<div class="text-[#FF0006]">${m}</div> `;
-      } else if (m.indexOf("[I]") !== -1) {
-        return `<div class="text-[#48BB31]">${m}</div> `;
-      } else if (m.indexOf("[D]") !== -1) {
-        return `<div class="text-[#0070BB]">${m}</div> `;
-      } else if (m.indexOf("[W]") !== -1) {
-        return `<div class="text-[#BBBB23]">${m}</div> `;
-      } else {
-        return `<div class="text-[#BBBBBB]">${m}</div> `;
-      }
-    });
+      .split("\n")
+      .filter(f => f)
+      .map(m => {
+        if (m.indexOf("[E]") !== -1) {
+          return `<div class="text-[#FF0006]">${m}</div> `;
+        } else if (m.indexOf("[I]") !== -1) {
+          return `<div class="text-[#48BB31]">${m}</div> `;
+        } else if (m.indexOf("[D]") !== -1) {
+          return `<div class="text-[#0070BB]">${m}</div> `;
+        } else if (m.indexOf("[W]") !== -1) {
+          return `<div class="text-[#BBBB23]">${m}</div> `;
+        } else {
+          return `<div class="text-[#BBBBBB]">${m}</div> `;
+        }
+      });
   return logs.reverse().join("");
 };
 
@@ -53,14 +53,18 @@ onUnmounted(() => {
 </script>
 <template>
   <div class="main">
-    <breadcrumb />
+    <breadcrumb/>
     <div class="app-container-breadcrumb">
       <div
-        class="w-full h-full p-4 bg-[#2B2B2B] rounded drop-shadow-lg overflow-y-auto"
-        v-html="loggerContent"
+          class="w-full h-full p-2 bg-[#2B2B2B] rounded drop-shadow-lg overflow-y-auto"
+          v-html="loggerContent"
       ></div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+::-webkit-scrollbar-track-piece {
+  background-color: transparent;
+}
+</style>
