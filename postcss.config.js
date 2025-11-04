b1617e8727525e833f89dddcd5ad7344ed505c99
刘嘉伟 <8473136@qq.com>
2025-11-04 17:23:08 +0800

    🔧 Remove unused autoprefixer and cssnano configurations from postcss.config.js to simplify the build process.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: {
     "@tailwindcss/postcss": {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {})
+    autoprefixer: {}
   }
 };
