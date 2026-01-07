c4e5d3a2d8bd04600901f391de9d382d0dfe5f79
刘嘉伟 <8473136@qq.com>
2026-01-07 14:10:23 +0800

    refactor: Consolidate timed event scheduling by removing the Job decorator and integrating its functionality into the Event decorator.

--- electron/event/monitor.ts
+++ electron/event/monitor.ts
@@ -3,10 +3,9 @@ import { inject, injectable } from "inversify";
 import BaseEvent from "../core/event";
 import SystemService from "electron/service/SystemService";
 import { TYPES } from "../di";
-import { Job } from "../core/decorators";
+import { Event } from "../core/decorators";
 import { BrowserWindow } from "electron";
 import { EventChannels } from "../core/constant";
-import ResponseUtils from "../utils/ResponseUtils";
 
 @injectable()
 export default class MonitorEvent extends BaseEvent {
@@ -18,13 +17,9 @@ export default class MonitorEvent extends BaseEvent {
         super(window);
     }
 
-    /**
-     * 系统监控定时任务 - 每秒发送一次系统使用情况
-     */
-    @Job({ name: 'systemMonitor', interval: 1000, immediate: true })
-    public async systemUsage() {
-        const usage = await this._systemService.getSystemUsage();
-        this.send(EventChannels.SYSTEM_MONITOR, ResponseUtils.success(usage));
+    @Event(EventChannels.SYSTEM_MONITOR, 1000)
+    public systemUsage() {
+        return this._systemService.getSystemUsage();
     }
 
 }
